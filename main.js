16e8f647fc0098d6475c1f15529f3a2e4dbe0c1e
Gerben Reppel <gerbenreppel@hotmail.com>
2020-09-03 10:55:13 +0200

    background + duck + hit detection + buggy movement
    de achtergrond staat op zijn plek. De eend staat op zijn plek. De achtergrond kan geraakt worden. Dit geeft een log in de console met "miss". De eend kan geraakt worden. Dit geeft een log in de console met "hit". Als de eend geraakt word dan beweegt hij.
    
    Dit klopt nog niet aangezien hij om een of andere reden "document.getElementById("duck").style.marginLeft" als undefined aangeeft maar als het word aangepast dan past de eend zich toch aan. Ook kan je na het aanpassen er blijkbaar ook niet meer mee rekenen??

---
+++ main.js
@@ -0,0 +1,31 @@
+var click = 0;
+var shots = 0;//houd je resterende kogels bij.
+var totalHits = 0;//houd bij hoeveel eenden je geraakt hebt.
+var score = 0;//houd je score bij.
+var direction = "margin-left";
+
+function miss() {//De functie voor als je je schot mist.
+
+	if (click == 1) { //Zorgt ervoor dat deze functie niet word uitgevoerd als er op de eend geklikt word.
+		click = 0;
+	}else{
+		console.log("miss");
+	}
+}
+
+function hit() {//de functie voor als je je schot raakt.
+	click = 1;
+	console.log("hit");
+
+	totalHits++
+	move();
+}
+
+function death() {//speelt de death animation van de eend af
+
+}
+
+function move() {
+	document.getElementById("duck").style.marginLeft = (document.getElementById("duck").style.marginLeft + "10px");
+	console.log(document.getElementById("duck").style.marginLeft)
+}//??????????????????????????????????????????????
