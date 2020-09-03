0f4230ada16f134ca55c64e53739aa8a9a685261
Gerben Reppel <gerbenreppel@hotmail.com>
2020-09-03 15:03:01 +0200

    eend vliegt. eend gaat dood. je kan falen.

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var click = 0;
 var shots = 0;//houd je resterende kogels bij.
 var totalHits = 0;//houd bij hoeveel eenden je geraakt hebt.
-var score = 0;//houd je score bij.
-var direction = "margin-left";
+var scorecount = 0;//houd je score bij.
+var directions = ["marginLeft", "marginTop"]
+var direction = "marginLeft";
+var eend = document.getElementById("duck");
+eend.style.marginLeft = "550px";
+eend.style.marginTop = "400px";
+document.getElementById("bullets").style.clip = "rect(0px,110px,25px,0px)";
 
 function miss() {//De functie voor als je je schot mist.
 
@@ -10,22 +15,44 @@ function miss() {//De functie voor als je je schot mist.
 		click = 0;
 	}else{
 		console.log("miss");
+		shooting()
 	}
 }
 
-function hit() {//de functie voor als je je schot raakt.
+function raak() {//de functie voor als je je schot raakt.
 	click = 1;
-	console.log("hit");
-
+	console.log("raak");
 	totalHits++
-	move();
+	eend.style.marginTop = "8400px";
 }
 
-function death() {//speelt de death animation van de eend af
-
+function shooting() {
+	shots++
+	if (shots == 1) {
+		document.getElementById("bullets").style.clip = "rect(0px,80px,25px,0px)";
+	}else if (shots == 2) {
+		document.getElementById("bullets").style.clip = "rect(0px,40px,25px,0px)";
+	}else {
+		alert("Je hebt al je kogels gebruikt")
+		location.reload();
+	}
 }
-var eend = document.getElementById("background");
-function move() {
-	eend.style.marginLeft = (eend.style.marginLeft + "10px");
-	console.log(document.getElementById("duck").style.marginLeft)
-}//??????????????????????????????????????????????
+
+
+setInterval(function move() {
+	var randomnumber = Math.floor(Math.random()*4);
+
+	if (randomnumber == 0) {
+		eend.style.marginLeft = parseInt(eend.style.marginLeft) + 10 + "px";
+	}else if (randomnumber == 1) {
+		eend.style.marginTop = parseInt(eend.style.marginTop) + 10 + "px";	
+	}else if (randomnumber == 2) {
+		eend.style.marginLeft = parseInt(eend.style.marginLeft) - 10 + "px";
+	}else if (randomnumber == 3) {
+		eend.style.marginTop = parseInt(eend.style.marginTop) - 10 + "px";
+	}
+	console.log(randomnumber)
+	console.log(eend.style.marginTop)
+	console.log(eend.style.marginLeft)
+},1500);
+
